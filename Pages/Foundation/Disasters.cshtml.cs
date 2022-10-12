73d10ce906e801d4759c68d8c0475b1b00537a5a
Reanetse Phofu <ST10128938@rcconnect.edu.za>
2022-10-12 14:33:19 +0200

    deleted class

--- Pages/Foundation/Disasters.cshtml.cs
+++ Pages/Foundation/Disasters.cshtml.cs
@@ -11,11 +11,47 @@ namespace MyWebsite.Pages.Foundation
     public class DisastersModel : PageModel
     {
         public DisasterInfo disasterInfo = new DisasterInfo();
+        public List<DisasterInfo> allDisasters = new List<DisasterInfo>();
 
         public string successMessage = "";
 
         public void OnGet()
         {
+            try
+            {
+                String connString = "Server=tcp:appr6312poe.database.windows.net,1433;Initial Catalog=Foundation;Persist Security Info=False;User ID=reanetse;Password=Momentox27p!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+
+                    string query1 = "select * from [dbo].[NewDisasters]";
+
+                    using (SqlCommand comm = new SqlCommand(query1, conn))
+                    {
+                        using (SqlDataReader reader = comm.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                DisasterInfo dis = new DisasterInfo();
+
+                                dis.did = "" + reader.GetInt32(0);
+                                dis.dlocation = reader.GetString(1);
+                                dis.dstart = reader.GetString(2);
+                                dis.dend = reader.GetString(3);
+                                dis.ddescription = reader.GetString(4);
+                                dis.daid = reader.GetString(5);
+
+                                allDisasters.Add(dis);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
         }
 
         public void OnPost()
--- Pages/Foundation/Index.cshtml.cs
+++ Pages/Foundation/Index.cshtml.cs
@@ -10,48 +10,11 @@ namespace MyWebsite.Pages.Foundation
 {
     public class IndexModel : PageModel
     {
-        public List<DisasterInfo> allDisasters = new List<DisasterInfo>();
         public List<GoodsInfo> listGoods = new List<GoodsInfo>();
         public List<MoneyInfo> listMoney = new List<MoneyInfo>();
 
         public void OnGet()
         {
-            try
-            {
-                String connString = "Server=tcp:appr6312poe.database.windows.net,1433;Initial Catalog=Foundation;Persist Security Info=False;User ID=reanetse;Password=Momentox27p!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-
-                using (SqlConnection conn = new SqlConnection(connString))
-                {
-                    conn.Open();
-
-                    string query1 = "select * from [dbo].[NewDisasters]";
-
-                    using (SqlCommand comm = new SqlCommand(query1, conn))
-                    {
-                        using (SqlDataReader reader = comm.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                DisasterInfo dis = new DisasterInfo();
-
-                                dis.did = "" + reader.GetInt32(0);
-                                dis.dlocation = reader.GetString(1);
-                                dis.dstart = reader.GetString(2);
-                                dis.dend = reader.GetString(3);
-                                dis.ddescription = reader.GetString(4);
-                                dis.daid = reader.GetString(5);
-
-                                allDisasters.Add(dis);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Exception: " + ex.ToString());
-            }
-
             try
             {
                 String connString = "Server=tcp:appr6312poe.database.windows.net,1433;Initial Catalog=Foundation;Persist Security Info=False;User ID=reanetse;Password=Momentox27p!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
