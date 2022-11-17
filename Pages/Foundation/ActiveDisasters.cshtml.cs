944f571c5b0b208379b14feb90b553ab35d6c777
Reanetse Phofu <ST10128938@rcconnect.edu.za>
2022-11-17 11:11:40 +0200

    complete code for active disasters page

--- Pages/Foundation/ActiveDisasters.cshtml.cs
+++ Pages/Foundation/ActiveDisasters.cshtml.cs
@@ -1,16 +1,55 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace MyWebsite.Pages.Foundation
 {
     public class ActiveDisastersModel : PageModel
     {
+
+        public DisasterInfo disasterInfo = new DisasterInfo();
+        public List<DisasterInfo> allDisasters = new List<DisasterInfo>();
+
+        public static DateTime Now { get; }
+
         public void OnGet()
         {
+            try
+            {
+                String connString = "Server=tcp:appr6312poe.database.windows.net,1433;Initial Catalog=Foundation;Persist Security Info=False;User ID=reanetse;Password=Momentox27p!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+
+                    string query1 = "select Location, StartDate, EndDate, Details, Aid from [dbo].[NewDisasters] where EndDate > '" + DateTime.Now + "';";
+
+                    using (SqlCommand comm = new SqlCommand(query1, conn))
+                    {
+                        using (SqlDataReader reader = comm.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                DisasterInfo dis = new DisasterInfo();
+
+                                dis.did = "" + reader.GetInt32(0);
+                                dis.dlocation = reader.GetString(1);
+                                dis.dstart = reader.GetDateTime(2);
+                                dis.dend = reader.GetDateTime(3);
+                                dis.ddescription = reader.GetString(4);
+                                dis.daid = reader.GetString(5);
+
+                                allDisasters.Add(dis);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
         }
     }
 }
